1ffca16b4401e7fe8f256b602ee4e5d3658df153
Hilton Santana Bastos <hiltonsantanabastos@gmail.com>
2024-10-08 14:08:19 -0300

    Upload desafio
    Desafio Concluído!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        Nokia nokia = new Nokia(numero: "123456", modelo: "Nokia 3310", imei: "111222333", memoria: 16);
+        nokia.Ligar();
+        nokia.InstalarAplicativo("Whatsapp");
+
+        Console.WriteLine("\n");
+
+        Iphone iphone = new Iphone(numero: "789012", modelo: "iPhone 13", imei: "444555666", memoria: 128);
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Telegram");
+    }
+}
